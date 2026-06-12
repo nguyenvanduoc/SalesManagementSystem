a7330dcf49740a48674b74b69609bf5c393cd4d1
nvduoc_dev <duoc01699232692@gmail.com>
2026-06-12 15:53:14 +0700

    ++ xóa xuất kho

--- SalesManagementSystem/App_Start/UnityConfig.cs
+++ SalesManagementSystem/App_Start/UnityConfig.cs
@@ -41,7 +41,6 @@ namespace SalesManagementSystem.App_Start
             container.RegisterType<SalesManagementSystem.Repositories.Interfaces.IDmKhoHangRepository, DmKhoHangRepository>(new HierarchicalLifetimeManager());
             container.RegisterType<SalesManagementSystem.Repositories.Interfaces.IDonDatHangRepository, DonDatHangRepository>(new HierarchicalLifetimeManager());
             container.RegisterType<SalesManagementSystem.Repositories.Interfaces.IPhieuNhapKhoRepository, SalesManagementSystem.Repositories.PhieuNhapKhoRepository>(new HierarchicalLifetimeManager());
-            container.RegisterType<SalesManagementSystem.Repositories.Interfaces.IPhieuXuatKhoRepository, SalesManagementSystem.Repositories.PhieuXuatKhoRepository>(new HierarchicalLifetimeManager());
             container.RegisterType<SalesManagementSystem.Repositories.Interfaces.INhaCungCapRepository, NhaCungCapRepository>(new HierarchicalLifetimeManager());
             container.RegisterType<SalesManagementSystem.Repositories.Interfaces.ITonKhoRepository, TonKhoRepository>(new HierarchicalLifetimeManager());
 
